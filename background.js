69405f9ac9689207162c6186a7519933804278af
habenzy <rstauss@burlingtoncodeacademy.com>
2022-03-17 19:07:13 -0400

    fixed timing reset bug

--- background.js
+++ background.js
@@ -3,35 +3,38 @@ let ticketTab = null;
 let ticketWindow = null;
 let ticketId = null;
 let currentUserEmail = null;
+let visitedURLs = [];
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   console.log(
     "ON UPDATED ----------------------------------" + changeInfo.status
   );
   console.log("tabId:", tabId);
+  visitedURLs.push(tab.url);
 
   if (
     !startTime &&
     tab.url &&
     tab.url.includes("https://app.hubspot.com") &&
-    tab.url.includes("8266889")
+    tab.url.includes("8266889") &&
+    tab.url.includes("ticket") &&
+    !tab.url.includes("?") && //This was an attempt at preventing a time reset on note or email open, it was unsuccessful
+    //because it resets when it leaves AND WHEN IT COMES BACK
+    //I need to make sure the previous URL also didn't include the app identifiers and a question mark
+    !(
+      visitedURLs[visitedURLs.length - 2].includes("?") &&
+      visitedURLs[visitedURLs.length - 2].includes("https://app.hubspot.com") &&
+      visitedURLs[visitedURLs.length - 2].includes("8266889")
+    )//It FUCKING WORKED!!!!!!!!
+    //Now to apply it to the fancy version and see if that works.
   ) {
     startTime = Date.now();
     ticketTab = tabId;
     ticketWindow = tab.windowId;
     ticketId = tab.url.match(/ticket\/(\d+)\D*$/)[1];
-  }
-  if (
-    !currentUserEmail &&
-    changeInfo.status === "complete" &&
-    tab.url &&
-    tab.url.includes("https://app.hubspot.com") &&
-    tab.url.includes("8266889")
-  ) {
-    getEmail(tab.id);
-  }
-  if (startTime && ticketTab === tabId && !tab.url.includes("ticket")) {
-    pushTime(ticketId, currentUserEmail)
+    if (!currentUserEmail) {
+      getEmail(tab.id);
+    }
   }
 });
 
@@ -50,6 +53,15 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
   }
 });
 
+async function getEmail() {
+  await chrome.identity.getProfileUserInfo(
+    { accountStatus: "ANY" },
+    (userInfo) => {
+      currentUserEmail = userInfo.email;
+    }
+  );
+}
+
 function pushTime(ticketId, email) {
   let end = Date.now();
   let duration = end - startTime;
@@ -57,6 +69,8 @@ function pushTime(ticketId, email) {
   let durationSecs = (duration / 1000) % 60;
   let durationMS = duration - durationMins * 60000 - durationSecs * 1000;
   console.log("submitting new log:");
+  console.log("start:", startTime);
+  console.log("end:", end);
   fetch("https://app.abodea.com/_hcms/api/time/record", {
     method: "POST",
     headers: new Headers({ "content-type": "application/json" }),
@@ -71,6 +85,11 @@ function pushTime(ticketId, email) {
     })
     .then((res) => {
       console.log("successfully tracked time, hubspot response:");
+      startTime = null;
+      ticketTab = null;
+      ticketWindow = null;
+      ticketId = null;
+      currentUserEmail = null;
       console.log(res);
     })
     .catch(
